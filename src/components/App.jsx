f7f022ff0de2114054e1e10b00f859cf04350943
Nadia Khoptiuk <holineinadia@gmail.com>
2022-07-05 11:07:42 +0300

    add start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,80 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Section from './Section';
+import FormAddContact from './FormAddContact';
+import ContactsList from './ContactsList';
+import FilterInput from './FilterInput';
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  checkContact = (contactObj, reset) => {
+    const nameToAdd = contactObj.name;
+    const { contacts } = this.state;
+
+    if (contacts.some(contact => contact.name === nameToAdd)) {
+      alert(`${nameToAdd} is already in your contacts`);
+      return;
+    }
+
+    this.handleSubmit(contactObj);
+    reset();
+  };
+
+  handleSubmit = contactObj => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contactObj],
+    }));
+  };
+
+  onInputFind = evt => {
+    const stringForFilter = evt.target.value;
+    this.setState({ filter: stringForFilter });
+  };
+
+  filterContacts = () => {
+    const { filter: stringForFilter, contacts } = this.state;
+    const normalizedString = stringForFilter.toLowerCase();
+
+    return stringForFilter
+      ? contacts.filter(contact =>
+          contact.name.toLowerCase().includes(normalizedString)
+        )
+      : contacts;
+  };
+
+  onContactDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const {
+      checkContact,
+      onInputFind,
+      filterContacts,
+      onContactDelete,
+      state,
+    } = this;
+    const filteredContacts = filterContacts();
+
+    return (
+      <>
+        <Section title="Phonebook">
+          <FormAddContact onSubmitForm={checkContact}></FormAddContact>
+        </Section>
+        <Section title="Contacts">
+          <FilterInput onFindContacts={onInputFind} state={state}></FilterInput>
+          <ContactsList
+            filteredContacts={filteredContacts}
+            deleteContact={onContactDelete}
+          ></ContactsList>
+        </Section>
+      </>
+    );
+  }
+}
